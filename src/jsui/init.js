5c6d9daa845c227c0957018064d204725428aaec
axilmar <axilmar@gmail.com>
2022-11-23 18:17:13 +0200

    Fix for avoiding vertical scrollbar in the whole of the page.

--- src/jsui/init.js
+++ src/jsui/init.js
@@ -1,10 +1,12 @@
 
 
 //make all styles use border-box, for easier size calculations
-document.head.insertAdjacentHTML("beforeEnd", "<style>*{box-sizing:border-box;}</style>");
+document.head.insertAdjacentHTML("beforeEnd", "<style>*{box-sizing:border-box}</style>");
 
 
-//make the html and body elements cover 100% of height
+//make the content cover the web page vertically 100% without scrollbars
 let htmlElement = document.getElementsByTagName("html")[0];
 htmlElement.style.height = "100%";
+htmlElement.style.margin = 0;
 document.body.style.height = "100%";
+document.body.style.margin = 0;
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import * as html from "./jsui/html.js";
 import * as layout from "./jsui/layout.js";
 
 let hbox1 = layout.vbox({parent: document.body});
-console.log(hbox1);
-
 let btn1 = html.button({parent: hbox1, innerHTML: "Button 1"});
 let btn2 = html.button({parent: hbox1, innerHTML: "Button 2", style: { flexGrow: 1}});
 let btn3 = html.button({parent: hbox1, innerHTML: "Button 3"});
+
+//let div1 = html.div({parent: document.body, style: { width: "10%", height: "100%", backgroundColor: "blue"}});
