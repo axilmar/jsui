d34cd6cad86808594e6646fded44c881a4a84994
axilmar <axilmar@gmail.com>
2022-11-24 01:44:24 +0200

    Applied externally given properties after setting the default properties, because the external properties might override the default ones.

--- src/jsui/layout.js
+++ src/jsui/layout.js
@@ -1,22 +1,24 @@
-import { applyProperties, addChildren } from "./util.js";
+import { applyProperties } from "./util.js";
 import { div } from "./html.js";
 
 
 export function hbox(properties, ...children) {
-    let resultElement = div(properties, ...children);
-    resultElement.className = "jsui-hbox";
+    let resultElement = div(null, ...children);
     resultElement.style.width = "100%";
     resultElement.style.display = "flex";
     resultElement.style.flexDirection = "row";
+    resultElement.className = "jsui-hbox";
+    applyProperties(resultElement, properties);
     return resultElement;
 }
 
 
 export function vbox(properties, ...children) {
-    let resultElement = div(properties, ...children);
-    resultElement.className = "jsui-vbox";
+    let resultElement = div(null, ...children);
     resultElement.style.height = "100%";
     resultElement.style.display = "flex";
     resultElement.style.flexDirection = "column";
+    resultElement.className = "jsui-vbox";
+    applyProperties(resultElement, properties);
     return resultElement;
 }
