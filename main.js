5ca5ef2557cf89adb912c2f169fa9d83ea1d7983
axilmar <axilmar@gmail.com>
2022-11-24 14:31:52 +0200

    Apparently, javascript imports are urls and not filesystem paths, and therefore putting the libraries outside of the root folder of the web app does not work...so these two files are placed in the root project folder, for now.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as html from "../src/html.js";
-import * as layout from "../src/layout.js";
+import * as html from "./src/html.js";
+import * as layout from "./src/layout.js";
 
-let hbox1 = layout.vbox({parent: document.body});
+let hbox1 = layout.hbox({parent: document.body});
 let btn1 = html.button({parent: hbox1, innerHTML: "Button 1"});
 let btn2 = html.button({parent: hbox1, innerHTML: "Button 2", style: { flexGrow: 1}});
 let btn3 = html.button({parent: hbox1, innerHTML: "Button 3"});
