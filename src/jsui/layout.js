e3f8be0afd5964084f9fbb46fbe84011db5fa714
axilmar <axilmar@gmail.com>
2022-11-24 14:11:43 +0200

    Added comments.

--- src/jsui/layout.js
+++ src/jsui/layout.js
@@ -3,22 +3,38 @@ import { div } from "./html.js";
 
 
 export function hbox(properties, ...children) {
+    //create the object with no properties
     let resultElement = div(null, ...children);
+    
+    //set the default properties
     resultElement.style.width = "100%";
     resultElement.style.display = "flex";
     resultElement.style.flexDirection = "row";
+    
+    //set the properties from css after setting the default properties
     resultElement.className = "jsui-hbox";
+    
+    //set the external properties after setting the default properties and the properties from css 
     applyProperties(resultElement, properties);
+    
     return resultElement;
 }
 
 
 export function vbox(properties, ...children) {
+    //create the object with no properties
     let resultElement = div(null, ...children);
+    
+    //set the default properties
     resultElement.style.height = "100%";
     resultElement.style.display = "flex";
     resultElement.style.flexDirection = "column";
+    
+    //set the properties from css after setting the default properties
     resultElement.className = "jsui-vbox";
+    
+    //set the external properties after setting the default properties and the properties from css 
     applyProperties(resultElement, properties);
+    
     return resultElement;
 }
