f35fe6210ff58eccfb14c32514c9cc91e18a3e38
axilmar <axilmar@gmail.com>
2022-12-01 14:06:48 +0200

    Added proper icon class.

--- main.js
+++ main.js
@@ -12,8 +12,17 @@ let btn3 = html.button({parent: hbox1, innerHTML: "Button 3"});
 //let div1 = html.div({parent: document.body, style: { width: "10%", height: "100%", backgroundColor: "blue"}});
 */
 
-let primaryButton = buttons.primaryButton({parent: document.body, disabled : false}, 
+/*
+let primaryButton = buttons.primaryButton({parent: document.body, disabled : true, classes: ""}, 
     icons.svgIcon({src: "/themes/material/icons/account.svg"}),
     "Primary Button");
+ */
 
 //icons.svgIcon({parent: document.body, src: "/themes/material/icons/account.svg", style:{width:"18px", height:"18px", color: "red"}});
+
+
+//html.img({parent: document.body, style: {width:"64px", height: "64px", maskImage: "url(/themes/material/icons/account.svg)", backgroundColor: "red"}});
+
+
+icons.icon({parent: document.body, src: "/temp/test.png"});
+icons.icon({parent: document.body, src: "/themes/material/icons/account.svg", style: {width:"32px", height:"32px", color: "red"}});
--- src/icons.js
+++ src/icons.js
@@ -1,5 +1,5 @@
 import { applyProperties } from "./util.js";
-import { div } from "./html.js";
+import { div, img } from "./html.js";
 
 
 export function svgIcon(properties, ...children) {
@@ -26,6 +26,51 @@ export function svgIcon(properties, ...children) {
     
     //set the class name
     resultElement.className = "svg-icon";
+    
+    //set the given properties
+    applyProperties(resultElement, properties);
+    
+    return resultElement;    
+}
+
+
+export function icon(properties, ...children) {
+    let resultElement = img(null, ...children);
+    
+    //redefine the property 'src' to check for if the value has the '.svg' suffix;
+    //if so, then set the style's mask-image property, otherwise call the original setter,
+    //or set the original value
+     Object.defineProperty(resultElement, "src", {
+       get: function() {
+           return Object.getOwnPropertyDescriptor(this.prototype, "src").get.call(this);
+       },
+       
+       set: function(v) {  
+           if (v.toLowerCase().endsWith(".svg")) {
+               let str = "url(" + v + ")";
+               this.style.maskImage = str;
+               this.style.webkitMaskImage = str;
+           }
+           else {
+               Object.getOwnPropertyDescriptor(Object.getPrototypeOf(this), "src").set.call(this, v);
+           }
+       }
+    });
+    
+    //wire the background color of style to color
+    //so as that setting the color changes the background color
+    Object.defineProperty(resultElement.style, "color", {
+        get: function() { 
+            return this.backgroundColor;
+        },
+        
+        set: function(v) {
+            this.backgroundColor = v;
+        }
+    });
+    
+    //set the class name
+    resultElement.className = "jsui-icon";
     
     //set the given properties
     applyProperties(resultElement, properties);
