0fc9338d15862082cfe0fcc2dba576b73affd670
axilmar <axilmar@gmail.com>
2025-01-31 18:23:25 +0200

    removed unnecessary code.

--- src/util.js
+++
@@ -1,6 +0,0 @@
-/**
-    Checks if an object is a string.
-    @param object object to check if it is a string.
-    @return true if the object is a string, false otherwise.
- */
-export const isString = (object) => typeof object === 'string' || object instanceof String;
